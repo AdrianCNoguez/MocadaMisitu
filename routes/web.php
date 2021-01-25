9b36e43802bca59cf75453e8b5c0d3582b2f9f50
Rodrigo Laguna <rodrigolaguna90@gmail.com>
2021-01-25 16:16:09 -0600

    Bocinas model routes

--- routes/web.php
+++ routes/web.php
@@ -73,11 +73,7 @@ Route::get('/decoracion_Hogar/maceta/{id}','MacetaController@viewProduct');
 Route::get('/difusores/humificadores','HumificadoresController@getProducts')->name("humificadores");
 Route::get('/difusores/humificador/{id}','HumificadoresController@viewProduct');
 /*--------Cocina-----------------*/
-<<<<<<< HEAD
-Route::get('disoensadores','DisoensadoresController@getProducts');
-=======
 Route::get('/cocina/disoensadores','DisoensadoresController@getProducts')->name("disoensadores");
 
 /*---------------Lamapras_pared-------------------*/
 //Route::get('/iluminacionInterior/lamparaspared','LamparasController@getProductsIntpared')->name('lamparaspared');
->>>>>>> 2f01888811b72a282f1f95b8bedd765cb94ee204
