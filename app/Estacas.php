d753a6f58f83ad7280fe331426e215d9fbb61ce5
JonathanLoredoTovar <jino9930opex@gmail.com>
2021-02-02 16:35:51 -0600

    Se modificó la seccion de estacas

--- app/Estacas.php
+++ app/Estacas.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estacas extends Model
 {
-    //
+    protected $table = 'estacas';
+    protected $primaryKey='idEstaca';
+
+    public static function imagenes($id)
+    {
+        $imagenes = Imagenes::select('ruta')->where('idProductoImagen_fk', '=', $id)->get();
+
+        return $imagenes;
+    }
 }
