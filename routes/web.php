d2d8747b94a9ef58b2d96e5a35d420cdfe0a0002
Rodrigo Laguna <rodrigolaguna90@gmail.com>
2021-02-05 18:03:39 -0600

    routes

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::get('/decoracion/proyectores/{id}','ProyectorController@viewProduct');
 Route::get('/decoracion/espejos','EspejoController@getProducts')->name('espejos');
 Route::get('/decoracion/espejos/{id}','EspejoController@viewProduct');
 
+
 Route::get('/decoracion/macetas','MacetasController@getProducts')->name('macetas');
 Route::get('/decoracion/macetas/{id}','MacetasController@viewProduct');
 /*----Exteriores-----------*/
