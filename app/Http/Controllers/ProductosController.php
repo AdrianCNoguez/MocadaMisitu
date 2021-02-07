15da199c709703d0d481153eab212b27ec22a796
unknown <titomane2d@gmail.com>
2021-02-06 21:26:40 -0600

    Prueba de paginacion :V

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -25,14 +25,18 @@ class ProductosController extends Controller
         $produc = Productos:: 
         select('producto.idProducto','producto.nombre','imagenes.ruta')
         ->join('imagenes','imagenes.idProductoImagen_fk','=','producto.idProducto')
+        //->groupBy('idProducto')->paginate(15);
         ->get()->groupBy('idProducto');
-        //$produc = Productos::paginate(5);
+        //$produc = Productos::paginate(10);
        
         return view('productos.productos')->with('produc',$produc);
     }
 
-    /*
+    
     public function viewProduct($id){
+        
+        $imagenes = Imagenes::select('ruta')->where('idProductoImagen_fk', '=', $id)->get();
+        return $imagenes;
 
         $position =  strpos($id, '-xs');
         $id = substr($id, $position + 3);
@@ -46,6 +50,6 @@ class ProductosController extends Controller
         return view('productos.producto')->with(compact('product', $produc))->with(compact('imagenes',$imagenes));
 
     }
-*/
+
     
 }
