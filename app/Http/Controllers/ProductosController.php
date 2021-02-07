b99553b49915fd3316a56a8e6054a2cc1f072885
unknown <titomane2d@gmail.com>
2021-02-07 02:19:49 -0600

    YA QUEDO KWN

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Imagenes;
 use App\Productos;
-use DB;
+use Illuminate\Support\Facades\DB;
+
 
 class ProductosController extends Controller
 {
@@ -20,47 +21,33 @@ class ProductosController extends Controller
     }
 
 
+
+    
     public function getViewProducts(){
         $modelos = ModelosController::obtenerModelos();
-        $produc = Productos:: 
-        select('producto.idProducto','producto.nombre','imagenes.ruta')
-        ->join('imagenes','imagenes.idProductoImagen_fk','=','producto.idProducto')
-        ->get()->groupBy('idProducto');
-        //$produc = Productos::paginate(5);
-        return view('productos.productos')->with('produc',$produc);
-    }
+        $collection = collect();
 
-    
-    public function viewProduct($id){
 
-        $position =  strpos($id, '-xs');
-        $id = substr($id, $position + 3);
+     for( $i = 0; $i < count($modelos) ; $i++ ) { 
 
-        $articulo = ModelosController::limpiarPeticion($articulo);
-
-        if ($articulo == 'error') {
-            return redirect()->back();
-        }else{
+        $productos = DB::table($modelos[$i])
+        ->join('producto','producto.idProducto','=', $modelos[$i].'.idProducto')->get();
             
-            for( $i = 0; $i < count($modelos) ; $i++ ) { 
-
-                $productos = DB::table($modelos[$i])
-                ->join('producto','producto.idProducto','=', $modelos[$i].'.idProducto')
-                ->where('producto.nombre','LIKE','%'. $articulo .'%')->get();
-                    
-                    if (!$productos->isEmpty()) {
-                        foreach ($productos as $producto) {
-                            $imagenes = Imagenes::where('idProductoImagen_fk',$producto->idProducto)->first();
-                            $producto->imagen = !is_null($imagenes) ? $imagenes->ruta: null;
-                            $collection->push($producto);
-                        }
-                    }
-                } 
-        }
-        
-        return view('search')->with(compact('collection', $collection)); 
-    } 
+            if (!$productos->isEmpty()) {
+                foreach ($productos as $producto) {
+                    $imagenes = Imagenes::where('idProductoImagen_fk',$producto->idProducto)->first();
+                    $producto->imagen = !is_null($imagenes) ? $imagenes->ruta: null;
+                    $collection->push($producto);
+                }
+            }
+        } 
+        //dd($produc);
     
+        return view('productos.productos')->with(compact('collection', $collection));
+     
+
+        
+    }
 
     
 }
