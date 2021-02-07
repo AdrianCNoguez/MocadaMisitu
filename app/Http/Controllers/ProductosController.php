8eed111bc1ae765c166c97a3878644342345eeae
unknown <titomane2d@gmail.com>
2021-02-06 20:30:30 -0600

    Se creo la vista de productos

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Productos;
+use DB;
 
 class ProductosController extends Controller
 {
@@ -17,4 +18,34 @@ class ProductosController extends Controller
             ->get()->groupBy('idProducto');
             return view('welcome')->with('produc',$produc);
     }
+
+
+    public function getViewProducts(){
+        
+        $produc = Productos:: 
+        select('producto.idProducto','producto.nombre','imagenes.ruta')
+        ->join('imagenes','imagenes.idProductoImagen_fk','=','producto.idProducto')
+        ->get()->groupBy('idProducto');
+        //$produc = Productos::paginate(5);
+       
+        return view('productos.productos')->with('produc',$produc);
+    }
+
+    /*
+    public function viewProduct($id){
+
+        $position =  strpos($id, '-xs');
+        $id = substr($id, $position + 3);
+
+        $product = Productos::
+        select('producto.idProducto')
+        ->where('producto.idProducto','=', $id)->first();
+
+        $imagenes = Productos::imagenesP($id);
+
+        return view('productos.producto')->with(compact('product', $produc))->with(compact('imagenes',$imagenes));
+
+    }
+*/
+    
 }
---
+++ resources/views/productos/productos.blade.php
@@ -0,0 +1,59 @@
+@extends('layout')
+@section('browser')
+    <style>
+        img{
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+    <link rel="stylesheet" href="{{ asset('css/browser-product.css') }}">
+@endsection
+
+@section('content')
+
+    <div class="container">
+        @section('breadcrumbs')
+        <li class="breadcrumb-item text-white ctive" aria-current="page">Lo más Vendido</li>
+        @endsection
+        @include('layouts.nav-pages')
+
+        
+
+        <div class="row justify-content-center justify-content-lg-start mb-2">
+           
+            @foreach ($produc as $item)
+            <div class="col-10 col-md-3 mb-3 no-gutters">
+                    <div class="card border-0 rounded-lg">
+                        <div class="img-container p-0">
+                            <a href="">
+                                <img src="{{ asset('img/'.$item->first()['ruta'] ) }}" alt="img"s class="obj-img">
+                            </a>
+                        </div>
+
+                        <div class="card-body pl-2 pr-2 pb-0 text-center">
+                            <p class="card-title font-weight-bold">{{ $item->first()['nombre'] }}</p>
+                            <a href="{{url('/producto/'.Str::slug($item->first()['nombre'].'-xs'.$item->first()['idProducto']))}}" class="btn green mb-3 text-white"><i class="fas fa-eye"></i>Ver detalles</a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+         
+
+
+        </div>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+                    </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
+
+    </div>
+@endsection
